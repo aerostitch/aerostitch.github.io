bc0ee192ec546487a4bfa712edc95a9f3fd9ff81
Joseph HERLANT <herlantj@gmail.com>
2013-09-27 23:06:04 +0200

    Automatic generation for menu.xml + corrections and modifications of certain files

--- _tools/build_menu_xml.rb
+++ _tools/build_menu_xml.rb
@@ -1,15 +1,49 @@
 #!/usr/bin/env ruby
 #
-# Going back from the tool dir to the root dir
-Dir.chdir('..')
 
-# Getting only html files in dirs not begining or ending by an underscore
-puts Dir.glob('**/index.html')
-        .delete_if{|fname| /(\/|^)_/.match(fname) or /_(\/|$)/.match(fname)}
-        .sort()
+require 'nokogiri'
 
-puts '******* Dirs *******'
-puts Dir.glob('**/*')
-        .delete_if { |fname| /(\/|^)_/.match(fname) or /_(\/|$)/.match(fname) }
-        .select { |filename| File.directory? filename }
+# This function builds recursively an xml containing all folders of the website
+def gen_subfolders_node(parent_dir, parent_node)
+  # Going back from the tool dir to the root dir
+  Dir.chdir(parent_dir) do
+    current_fullpath = Dir.pwd
+    
+    # building tree
+    subfolders = Dir.glob('*')
+      .delete_if { |fname| /(\/|^)_/.match(fname) or /_(\/|$)/.match(fname) }
+      .select { |filename| File.directory? filename }
+      .each { |fold|
+        # child = parent_node.add_element 'url';
+        # ti = child.add_element 'loc';
+        # ti.text = fold;
+        parent_node.send('url') do
+          parent_node.title(fold.capitalize)
+          parent_node.path(fold)
+          gen_subfolders_node("#{current_fullpath}/#{fold}", parent_node);
+        end
+      }
+  end
+end
 
+# Process starts here...
+doc = Nokogiri::XML::Builder.new do |xml|
+  xml.urlset do
+    xml.url {
+      xml.title "Home"
+      xml.path ""
+    }
+    gen_subfolders_node('..', xml)
+  end
+end
+
+# Finally writing menu.xml file
+begin
+  file = File.open("../menu.xml", "w")
+  file.write(doc.to_xml)
+rescue IOError => e
+  puts "[Error] Could not open file."
+  puts e.message
+ensure
+  file.close unless file == nil
+end
--- custom_/js/custom_jquery_twbs.js
+++ custom_/js/custom_jquery_twbs.js
@@ -4,55 +4,78 @@
  * https://aerostitch.github.io
  *
  * Description:
+ * This function generates some parts of the page of my site
+ * using twitter bootstrap 3.0.0
+ */
+$(document).ready(function(){
+  build_top_menu();
+  build_index_folder_content();
+});
+
+/*
  * This function generates the menu of the page using twitter bootstrap 3.0.0
  * It uses a "menu.xml" file
  */
-$(document).ready(function(){
+function build_top_menu(){
   $.ajax({
     type: "GET" ,
     url: "/menu.xml" ,
     dataType: "xml" ,
-    success: build_top_menu
+    success: build_topleft_menu
   });
-});
+}
 
 /*
  * This function is the one that really generates the menu
- * from the content of the xml file
+ * from the content of the menu.xml file
  */
-function build_top_menu(xml){
+function build_topleft_menu(xml){
   $(xml).find('urlset > url').each(function(){
     var item_title = $(this).find('> title').text();
-    var item_url = $(this).find('> loc').length ? $(this).find('> loc').text() : "";
-    var a_elt = null;
+    var item_url = $(this).find('> path').length ? $(this).find('> path').text() : "";
     var add_elt =  null;
+
     if ($(this).find('> url').length > 0){
-      var ul_elt = $('<ul>').attr('class','dropdown-menu');
-      $(this).find('> url').each(function(){
-        var s_title = $(this).find('> title').text();
-        var s_url = $(this).find('> loc').length ? $(this).find('> loc').text() : "";
-        ul_elt.append(
-          $('<li>').append(
-            $('<a>').attr('href',s_url).append(s_title)
-          )
-        );
-      });
-      a_elt =  
-        $('<a>').attr('href','#')
-          .attr('class','dropdown-toggle')
-          .attr('data-toggle','dropdown')
-          .append(item_title)
-          .append($('<b>').attr('class','caret'));
-      add_elt = $('<li>').attr('class','dropdown')
-          .append(a_elt)
-          .append(ul_elt);
+      add_elt = add_level2_menu_node($(this), item_title, item_url);
     } else { 
-      a_elt = $('<a>').attr('href',item_url).append(item_title);
-      add_elt = $('<li>').append(a_elt);
-      if($(location).attr('href') == item_url){add_elt.attr('class','active');}
+      add_elt = build_menu_item(item_title, item_url)
     }
     $('#menu_left').append(add_elt);
   });
+}
+
+/*
+ * This function builds up the dropdown contents of the menu
+ */
+function add_level2_menu_node(xml, p_title, p_folder){
+  var ul_elt = $('<ul>').attr('class','dropdown-menu')
+                        .append(build_menu_item(p_title +' tips', p_folder));
+  $(xml).find('> url').each(function(){
+    var s_title = $(this).find('> title').text();
+    var s_url = p_folder +'/';
+    s_url += $(this).find('> path').length ? $(this).find('> path').text() : "";
+    ul_elt.append(
+      build_menu_item(s_title, s_url)
+    );
+  });
+  var a_elt =  
+    $('<a>').attr('href','/'+ p_folder)
+      .attr('class','dropdown-toggle')
+      .attr('data-toggle','dropdown')
+      .append(p_title)
+      .append($('<b>').attr('class','caret'));
+  return $('<li>').attr('class','dropdown')
+      .append(a_elt)
+      .append(ul_elt);
+}
+
+/*
+ * This function builds an atomic node of the menu
+ */
+function build_menu_item(title, url){
+  var elt = $('<li>').append($('<a>').attr('href','/' + url).append(title));
+  if($(location).attr('href') == url){elt.attr('class','active');}
+  return elt
 }
 
 /* 
@@ -78,13 +101,13 @@ function build_index_folder_content(){
  */
 function add_index_child_url(xml, parent_obj){
   $(xml).find('> url').each(function(){
-  var item_title = $(this).find('> title').text();
-  var item_url = $(this).find('> path').length ? $(this).find('> path').text() : "#";
-  var current_obj = add_index_page_list_item(item_title, item_url);
-  if($(this).find('> url').length > 0){
-    add_index_child_url($(this), current_obj.append($('<u>')));
-  }
-  parent_obj.append(current_obj);
+    var item_title = $(this).find('> title').text();
+    var item_url = $(this).find('> path').length ? $(this).find('> path').text() : "#";
+    var current_obj = add_index_page_list_item(item_title, item_url);
+    if($(this).find('> url').length > 0){
+      add_index_child_url($(this), current_obj.append($('<u>')));
+    }
+    parent_obj.append(current_obj);
   });
 }
 
