9912dc5f382052d4671048c9c67ac181cf88b279
Joseph HERLANT <herlantj@gmail.com>
2013-10-09 04:48:44 +0200

    Using now the revision date as last update date for sitemap.xml generation

--- _tools/build_sitemap_xml.rb
+++ _tools/build_sitemap_xml.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 #
+require 'date'
 require 'time'
 require 'nokogiri'
 
@@ -7,6 +8,15 @@ SiteUrl = 'http://aerostitch.github.io'
 # This is to ensure we are in the same directory as the script
 # to enable people to run the script from another directory
 script_path = File.dirname(File.expand_path($0))
+
+# This function will get the content of the revdate in the html documents
+# Returns a date if a revdate tag is found, else returns nil. 
+def get_revdate(doc_path)
+  doc = Nokogiri::HTML(File.open(doc_path)) { |config| config.strict.nonet}
+  revdate = doc.search('//span[@id="revdate"]').text.strip
+  DateTime.strptime(revdate, '%Y-%m-%d') if /^\d{4}-\d{2}-\d{2}$/.match(revdate)
+end
+
 Dir.chdir(script_path)
 
 # Process starts here...
@@ -27,7 +37,14 @@ doc = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
           if file != fdesc and File.exists?(fdesc)
             xml.lastmod((File.new(fdesc).mtime).iso8601)
           else
-            xml.lastmod((File.new(file).mtime).iso8601)
+            # If a revision date is found in the doc, use it.
+            # Else, use the file last modification date
+            dte = get_revdate(file)
+            if dte.nil?
+              xml.lastmod((File.new(file).mtime).iso8601)
+            else
+              xml.lastmod((get_revdate(file)).iso8601)
+            end
           end
           xml.changefreq('monthly')
           xml.priority('1.0')
