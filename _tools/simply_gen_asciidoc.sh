65b1d9e4dd39c3391a9f769ab56b54e647ccbd12
Joseph HERLANT <herlantj@gmail.com>
2013-10-09 05:50:18 +0200

    Modifying headers for html5, correcting double border issue for code blocks, correcting non closed tags, adding keyword tag to text files

--- _tools/simply_gen_asciidoc.sh
+++ _tools/simply_gen_asciidoc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # This is just to regenerate asciidocs while creating the backend.
 # Then Rasciidoc (https://github.com/llicour/raskiidoc) will be used.
-find ../ -name '*.txt' -and -not -name 'Notes.txt' -exec asciidoc --conf-file=asciidoc-twbs-backend.conf {} \;
+find ../ -name '*.txt' -and -not -name 'Notes.txt' -exec asciidoc \
+--conf-file=asciidoc-twbs-backend.conf -b html5 {} \;
