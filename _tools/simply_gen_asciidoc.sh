b19a3188e08b4fb1dcab69406431d5b303b1c59e
Joseph HERLANT <herlantj@gmail.com>
2013-10-10 12:27:28 +0200

    Adding content and regenerating files accordingly

--- _tools/simply_gen_asciidoc.sh
+++ _tools/simply_gen_asciidoc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # This is just to regenerate asciidocs while creating the backend.
 # Then Rasciidoc (https://github.com/llicour/raskiidoc) will be used.
-find ../ -name '*.txt' -and -not -name 'Notes.txt' -exec asciidoc \
---conf-file=asciidoc-twbs-backend.conf -b html5 {} \;
+find ../ -name '*.txt' -and -not -name 'Notes.txt' -and -not -name 'doc_template.txt' \
+-exec asciidoc --conf-file=asciidoc-twbs-backend.conf -b html5 {} \;
