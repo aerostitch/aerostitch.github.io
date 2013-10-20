33ef473d5960238e06dafe1acebf79140d698d85
Joseph HERLANT <herlantj@gmail.com>
2013-10-20 11:40:57 +0200

    Modifying asciidoc files extension from txt to adoc

--- _tools/simply_gen_asciidoc.sh
+++ _tools/simply_gen_asciidoc.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # This is just to regenerate asciidocs while creating the backend.
 # Then Rasciidoc (https://github.com/llicour/raskiidoc) will be used.
-find ../ -name '*.txt' -and -not -name 'Notes.txt' -and -not -name 'doc_template.txt' \
--exec asciidoc --conf-file=asciidoc-twbs-backend.conf -b html5 {} \;
+find ../ -name '*.adoc' -and -not -name 'Notes.adoc' -and -not -name \
+'doc_template.adoc' -exec asciidoc --conf-file=asciidoc-twbs-backend.conf \
+-b html5 {} \;
+
 ruby build_index_files.rb 
 ruby build_menu_xml.rb
 ruby build_sitemap_xml.rb 
