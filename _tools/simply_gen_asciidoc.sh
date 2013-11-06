118916fa30c62da05c8e5309ce09454a487c6409
Joseph HERLANT <herlantj@gmail.com>
2013-11-06 18:59:27 +0100

    Configuring the submodule

--- _tools/simply_gen_asciidoc.sh
+++ _tools/simply_gen_asciidoc.sh
@@ -2,7 +2,7 @@
 # This is just to regenerate asciidocs while creating the backend.
 # Then Rasciidoc (https://github.com/llicour/raskiidoc) will be used.
 find ../ -name '*.adoc' -and -not -name 'Notes.adoc' -and -not -name \
-'doc_template.adoc' -exec asciidoc --conf-file=asciidoc-twbs-backend.conf \
+'doc_template.adoc' -exec asciidoc --conf-file=asciidoc-twbs-backend/asciidoc-twbs-backend.conf \
 -b html5 {} \;
 
 ruby build_index_files.rb 
